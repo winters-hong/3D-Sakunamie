5e4c4c0020d2313b470765bb3cf75019e409cfb5
winters-hong <116630156+winters-hong@users.noreply.github.com>
2024-06-13 15:11:53 +0900

    Add files via upload

---
+++ Scripts/weatherInfo/WeatherInfoController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeatherInfoController : MonoBehaviour
+{
+    public Text cityText;
+    public Text weatherText;
+    public Text tempText;
+    public Text noticeText;
+    public Text dateText;
+
+    public string cityInfo;
+    public string weatherInfo;
+    public string tempInfo;
+    public string noticeInfo;
+    public string dateInfo;
+
+    /// <summary>
+    /// Access location info from IP address
+    /// </summary>
+    string Posurl =
+        "http://api.map.baidu.com/location/ip?ak=wHiiAx78NfeHj69MpsXgzberonXEFBhM&coor=bd09ll";
+
+    /// <summary>
+    /// Get weather info
+    /// </summary>
+    string Weatherurl = "http://t.weather.sojson.com/api/weather/city/";
+
+    void Start()
+    {
+        //Request for location info
+        StartCoroutine(RequestPos());
+    }
+
+    IEnumerator RequestPos()
+    {
+        WWW www = new WWW(Posurl);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            positionTool.ResponseBody t = LitJson.JsonMapper.ToObject<positionTool.ResponseBody>(
+                www.text
+            );
+            Debug.Log(t.content.address_detail.city);
+
+            //Attach weather info to string var
+            //cityText.text = t.content.address_detail.city;
+            cityInfo = t.content.address_detail.city;
+            StartCoroutine(RequestWeather(weatherTool.GetWeatherId(t.content.address_detail.city)));
+        }
+    }
+
+    IEnumerator RequestWeather(int id)
+    {
+        WWW www = new WWW(Weatherurl + id.ToString());
+        //Debug.Log(Weatherurl + id.ToString());
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            //Debug.Log(www.text);
+            weatherTool.WeathBody t = LitJson.JsonMapper.ToObject<weatherTool.WeathBody>(www.text);
+            Debug.Log(t.data.forecast[0].type);
+
+            // dateText.text = t.date;
+            // weatherText.text = t.data.forecast[0].type;
+            // tempText.text = t.data.forecast[0].low + "°C" + "~" + t.data.forecast[0].high + "°C";
+            // noticeText.text = t.data.forecast[0].notice;
+
+            dateInfo = t.date;
+            weatherInfo = t.data.forecast[0].type;
+            tempInfo = t.data.forecast[0].low + "°C" + "~" + t.data.forecast[0].high + "°C";
+            noticeInfo = t.data.forecast[0].notice;
+        }
+    }
+}
---
+++ Scripts/weatherInfo/weatherTool.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weatherTool
+{
+    public static Dictionary<string, int> PosToId = new Dictionary<string, int>();
+
+    public static bool initDic = false;
+
+    public static int GetWeatherId(string name)
+    {
+        int id = 0;
+        if(!initDic)
+        {
+            initDic = true;
+            TextAsset ta = Resources.Load<TextAsset>("WeatherId");
+            List<Pos2Id> temp = LitJson.JsonMapper.ToObject<List<Pos2Id>>(ta.text);
+            foreach(Pos2Id t in temp)
+            {
+                PosToId[t.placeName] = t.id;
+            }
+        }
+        for(int i=1;i<name.Length;i++)
+        {
+            string tn = name.Substring(0, i);
+            if(PosToId.ContainsKey(tn))
+            {
+                id = PosToId[tn];
+            }
+        }
+        return id;
+    }
+
+    public class Pos2Id
+    {
+        public string placeName;
+        public int id;
+
+        public Pos2Id()
+        {
+
+        }
+        public Pos2Id(string name,int id)
+        {
+            placeName = name;
+            this.id = id;
+        }
+    }
+
+    public class WeathBody
+    {
+        public string time;
+        public CityInfo cityInfo;
+        public string date;
+        public string message;
+        public int status;
+        public WeathData data;
+    }
+
+    public class CityInfo
+    {
+        public string city;
+        public string cityId;
+        public string parent;
+        public string updateTime;
+    }
+
+    public class WeathData
+    {
+        public string shidu;
+        public double pm25;
+        public double pm10;
+        public string quality;
+        public string wendu;
+        public string ganmao;
+        public WeathDetailData yesterday;
+        public WeathDetailData[] forecast;
+    }
+
+    public class WeathDetailData
+    {
+        public string date;
+        public string sunrise;
+        public string high;
+        public string low;
+        public string sunset;
+        public double aqi;
+        public string ymd;
+        public string week;
+        public string fx;
+        public string fl;
+        public string type;
+        public string notice;
+    }
+
+}
